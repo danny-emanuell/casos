1c9d5d4c88842ffd7069c6911a0f18fe0058ea38
Danny Alvarez <dannyoemanuell@gmail.com>
2017-12-03 23:05:07 -0600

    ya se agrego el localstorage solo para agregar el caso, queda pendiente que guarde en el localstorage todo el seguimiento.

--- src/app/services/datacases/datacases.service.ts
+++ src/app/services/datacases/datacases.service.ts
@@ -1,32 +1,73 @@
 import { Injectable } from '@angular/core';
 import { ICase } from './../datacases.interfaces';
+import * as localStorage from 'localstoragedb';
 
 @Injectable()
 export class DatacasesService {
 
   Cases: ICase[] = [];
 
-  constructor() { }
+  private tables = {
+    cases: {
+      nameTable: 'cases',
+      nameColumns: [
+        '_caseDate',
+        '_caseDescription',
+        '_caseStatus',
+        '_customerID',
+        '_customerName',
+        '_noOrder',
+        '_tracking'
+      ]
+    }
+  }
+
+  public nameDatabase = 'caseDB';
+
+  private casesDB = new localStorage( this.nameDatabase , localStorage );
+
+  constructor() {
+    this.createTable();
+    this.getCases();
+   }
+
+  private createTable (){
+    const cases = this.casesDB;
+    if( cases.isNew() ) {
+      cases.createTable( this.tables.cases.nameTable , this.tables.cases.nameColumns );
+      cases.commit();
+    }
+  }
 
   addCase( ncase: ICase ){
+
+    const cases = this.casesDB;
+
     return new Promise(( resolve , reject ) => {
-      if( this.Cases.unshift(ncase) ) {
+
+      try {
+        cases.insert( this.tables.cases.nameTable , ncase );
+        cases.commit();
+        this.Cases.unshift( ncase );
         resolve({
           code: 200,
           message: 'Se guardo correctamente'
         });
-      } else {
+      } catch( error ) {
         reject( {
           code: 501,
           message: 'Error al guardar la información'
         });
       }
-    })
-  };
+  });
 
-  getCases(){
-    return this.Cases;
   }
+
+  getCases() {
+    const cases = this.casesDB;
+    return this.Cases = cases.queryAll( this.tables.cases.nameTable );
+  }
+  /*
 
   deleteCase( caseToDelete: ICase ) {
     for ( let i = 0; i < this.Cases.length; i++) {
@@ -36,5 +77,5 @@ export class DatacasesService {
     }
   }
 
-
+*/
 };
--- src/app/services/localstorage/localstorage.service.ts
+++ src/app/services/localstorage/localstorage.service.ts
@@ -4,6 +4,48 @@ import * as localStorage from 'localstoragedb';
 @Injectable()
 export class LocalstorageService {
 
+  private tables = {
+    cases: {
+      nameTable: 'cases',
+      nameColumns: [
+        '_idcase', 
+        'date',
+        '_user',
+        '_idCaese',
+        'number',
+        '_noOrder',
+        '_customerName',
+        '_customerID',
+        '_caseDate',
+        'number',
+        '_caseStatus',
+        '_caseDescription',
+        '_tracking'
+      ]
+    }
+  }
+
+  private casesDB = new localStorage( 'caseDB', localStorage );
+
   constructor() { }
+
+  private createTable() {
+    const cases = this.casesDB;
+    if( cases.isNew() ) {
+      cases.createTable( this.tables.cases.nameTable , this.tables.cases.nameColumns );
+      cases.commit();
+    }
+  }
+
+  insertData( data ) {
+    const cases = this.casesDB;
+    cases.insert( this.tables.cases.nameTable , data );
+    cases.commit();
+  }
+
+  getData(){
+    const cases = this.casesDB;
+    return cases.queryAll( this.tables.cases.nameTable );
+  }
 
 }
